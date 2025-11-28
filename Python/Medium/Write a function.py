91d0aa8550fe3fd70f56ed98e484ca22b36933c5
Bhavesh Harmalkar <78724413+bhaveshharmalkar@users.noreply.github.com>
2025-11-28 21:28:59 +0530

    Add leap year checking function
    Implement function to check for leap years.

---
+++ Python/Medium/Write a function.py
@@ -0,0 +1,17 @@
+def is_leap(year):
+    if (year % 100 == 0):
+        if (year % 400 == 0):
+            leap = True
+            return leap
+        else:
+            leap = False
+            return leap
+    elif (year % 4 == 0):
+        leap = True
+        return leap
+    else:
+        leap = False
+        return leap
+        
+year = int(input())
+print(is_leap(year))
